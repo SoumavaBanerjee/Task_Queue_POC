8ded54e2a8fd14c45499b148a7d9f088eab5f12f
Soumava Banerjee <soumava.rivu@gmail.com>
2023-08-29 19:35:09 +0530

    initial commit 🚀

---
+++ Client/src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+  const handleClick = () => {
+    console.log("hello");
+  };
+
+  return (
+    <>
+      <h2>Hit this button to start an async task</h2>
+      <div>
+        <button onClick={handleClick}>Create a task</button>
+      </div>
+
+      <div className="progressbar">
+        <progress value={0} max={100} />
+      </div>
+    </>
+  );
+}
+
+export default App;
